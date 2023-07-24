6f7ed28bfa35e8741b0f9073f8e514834500dcc1
Younghyun Lee <younghyunlee22@gmail.com>
2023-07-24 12:55:17 -0500

    jsonwebtoken auth-check admin-check

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 import { requireSignin, isAdmin } from "../middlewares/auth.js";
-import { register, login } from "../controllers/auth.js";
+import { register, login, secret } from "../controllers/auth.js";
 
 router.post("/register", register);
 router.post("/login", login);
